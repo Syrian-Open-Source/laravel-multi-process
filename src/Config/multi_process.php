1c29642f833b9c1a00ff68004485aaf5bc222c06
karam  mustafa <karam2mustafa@gmail.com>
2021-12-22 23:46:08 +0200

    wip

---
+++ src/Config/multi_process.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Options
+    |--------------------------------------------------------------------------
+    | This are the configs and options we passing to
+    | the symfony process component. You can edit them from here
+    | you can change them directly while you execute the code by setOption function.
+    */
+    'options' => [
+
+        // the timeout time that symfony process component
+        // trying to execute the each command.
+        'timeOut' => 60,
+
+        // the ide timeout time that symfony process component
+        // trying to execute the each command.
+        'ideTimeOut' => 60,
+
+        // the initial work directory, the default is base_bath()
+        'workingDirectory' => null,
+
+        // dump output if you are using run method to execute your tasks
+        'enableOutput' => true,
+
+        // The time in seconds that the package tries to perform the un executed tasks.
+        'processTime' => 3,
+
+    ],
+];
